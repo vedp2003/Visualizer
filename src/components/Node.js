6fe0272571af39b928581299b3ed6bcbae3154e7
vedp2003 <vedp2003@gmail.com>
2024-12-28 21:26:59 -0500

    new

--- src/components/Node.js
+++ src/components/Node.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../styles/Node.css";
 import { motion } from "framer-motion";
 
-export default function Node({ value, next, color }) {
+export default function Node({ value, prev, next, color }) {
   return (
     <motion.div
-      className="node"
+      className="singly-node"
       style={{ background: color }}
-      initial={{ opacity: 0, scale: 1 }}
+      initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.8 }}
+      exit={{ opacity: 0, scale: 0 }}
     >
       <p>{`Value: ${value}`}</p>
       <p>{`Next: ${next?.value || "null"}`}</p>
